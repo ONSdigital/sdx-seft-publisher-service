3ae23bb3878d0bf7f7eac36b266c1a8f89d148e7
Warren Bailey <warren@warrenbailey.net>
2017-05-15 11:03:41 +0100

    Go rountines with channels

--- publisher.go
+++ publisher.go
@@ -10,6 +10,12 @@ import (
 	"bytes"
 )
 
+type File struct {
+	buf []byte
+	fileName string
+	respCode int
+}
+
 func handleError(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -18,53 +24,81 @@ func handleError(err error) {
 
 func poll_ftp() {
 	fmt.Println("Poll ftp")
-	conn, err := ftp.Connect("localhost:2021")
-	handleError(err)
-
-	conn.Login("ons", "ons")
+	conn := connectToFtp()
 	files, err := conn.List("/")
 	handleError(err)
+	defer conn.Logout()
+	defer conn.Quit()
 
 	for _, file := range  files {
 		fmt.Println("File name " + file.Name)
-		go readAndPublish(file)
+		readChannel := read(file)
+		publishChannel := publish(readChannel)
+		delete(publishChannel)
 	}
-	conn.Logout()
-	conn.Quit()
 }
 
-func readAndPublish(file *ftp.Entry) {
+func read(file *ftp.Entry) <- chan File {
+	out := make(chan File)
+	go func() {
+		conn := connectToFtp()
+		defer conn.Logout()
+		defer conn.Quit()
+
+		content, err := conn.Retr(file.Name)
+		defer content.Close()
+		handleError(err)
+		buf, err := ioutil.ReadAll(content)
+		res := File{buf, file.Name, 0}
+		handleError(err)
+		out <- res
+		close(out)
+	}()
+	return out
+}
+
+func publish(in <- chan File) <- chan File {
+	out := make(chan File)
+	go func () {
+		for file := range in {
+
+			fmt.Println("Publishing file")
+			resp, err := http.Post("http://localhost:8080/upload/bres/1/"+ file.fileName,
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+				bytes.NewReader(file.buf))
+			handleError(err)
+			result, err := ioutil.ReadAll(resp.Body)
+			handleError(err)
+			resp.Body.Close()
+			fmt.Printf("%s", result)
+			file.respCode = resp.StatusCode
+			out <- file
+		}
+		close(out)
+	}()
+	return out
+
+}
+
+func delete(in <- chan File) {
+	go func() {
+		conn := connectToFtp()
+		defer conn.Logout()
+		defer conn.Quit()
+		for file := range in {
+			if file.respCode == 200 {
+				err := conn.Delete(file.fileName)
+				handleError(err)
+			}
+		}
+	}()
+}
+func connectToFtp() *ftp.ServerConn {
 	conn, err := ftp.Connect("localhost:2021")
 	handleError(err)
 	conn.Login("ons", "ons")
-	content, err := conn.Retr(file.Name)
-	handleError(err)
-	buf, err := ioutil.ReadAll(content)
-	handleError(err)
-	content.Close()
-	publish(file.Name, buf, conn)
-	conn.Logout()
-	conn.Quit()
-}
 
-func publish(fileName string, buf []byte, conn *ftp.ServerConn) {
-	fmt.Println("Publishing file")
-	resp, err := http.Post("http://localhost:8080/upload/bres/1/" + fileName,
-		"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-		bytes.NewReader(buf))
-	handleError(err)
-	result, err := ioutil.ReadAll(resp.Body)
-	handleError(err)
-	resp.Body.Close()
-	fmt.Printf("%s", result)
-	if resp.StatusCode == 200 {
-		delete(fileName, conn)
-	}
-}
-
-func delete(fileName string, conn *ftp.ServerConn) {
-	err := conn.Delete(fileName)
-	handleError(err)
+	return conn
 }
 
 func main() {
