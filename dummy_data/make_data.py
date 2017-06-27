88d13f7277f92ece689e2c4d8380dedf0fa9aa31
Andrew Millar <andrew.millar@ons.gov.uk>
2017-06-27 13:36:36 +0100

    updating to include multipart form (not working)

--- dummy_data/make_data.py
+++
@@ -1,7 +0,0 @@
-import xlsxwriter
-
-for x in range(111, 222):
-    workbook = xlsxwriter.Workbook('book{}.xlsx'.format(x))
-    worksheet = workbook.add_worksheet()
-    worksheet.write('A1', 'Hello, World!')
-    workbook.close()
