db1ae6c1d84e89e172a43784e005d37959a2d7b3
Dave Haynes <david.haynes@ons.gov.uk>
2017-07-12 09:31:15 +0100

    Added logging configuration.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+import argparse
 from collections import deque
+import logging
+from logging.handlers import WatchedFileHandler
+import sys
 
 import tornado.ioloop
 import tornado.web
@@ -9,7 +13,7 @@ from ftpclient import FTPWorker
 class StatusService(tornado.web.RequestHandler):
 
     def initialize(self, work):
-        self.recent = {n: v._asdict() for n, v in enumerate(work.recent)}
+        self.recent = {n: v._asdict() for n, v in enumerate(Work.recent)}
 
     def get(self):
         self.write(self.recent)
@@ -19,7 +23,9 @@ class Work:
     recent = deque(maxlen=24)
 
     @classmethod
-    def transfer_task(cls, loop):
+    def transfer_task(cls):
+        log = logging.getLogger("sdx.seft")
+        log.info("Looking for files...")
         worker = FTPWorker(**params)
         # publisher = ?
         with worker as active:
@@ -41,7 +47,42 @@ def make_app():
         (r"/recent", StatusService, {"work": Work}),
     ])
 
-if __name__ == "__main__":
+
+def parser(description="SEFT Publisher service."):
+    p = argparse.ArgumentParser(description)
+    p.add_argument(
+        "-v", "--verbose", required=False,
+        action="store_const", dest="log_level",
+        const=logging.DEBUG, default=logging.INFO,
+        help="Increase the verbosity of output")
+    p.add_argument(
+        "--log", default=None, dest="log_path",
+        help="Set a file path for log output")
+    return p
+
+def main(args):
+    log = logging.getLogger("sdx.seft")
+    log.setLevel(args.log_level)
+
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)-7s %(name)s|%(message)s")
+    ch = logging.StreamHandler()
+
+    if args.log_path is None:
+        ch.setLevel(args.log_level)
+    else:
+        fh = WatchedFileHandler(args.log_path)
+        fh.setLevel(args.log_level)
+        fh.setFormatter(formatter)
+        log.addHandler(fh)
+        ch.setLevel(logging.WARNING)
+
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+    log = logging.getLogger("pika")
+    log.setLevel(args.log_level)
+    log.addHandler(ch)
+
     app = make_app()
     app.listen(8888)
     interval_ms = 30 * 60 * 1000
@@ -51,4 +92,13 @@ if __name__ == "__main__":
     )
     # start your period timer
     sched.start()
+    log = logging.getLogger("sdx.seft")
+    log.info("Scheduler started.")
     tornado.ioloop.IOLoop.current().start()
+    return 0
+
+if __name__ == "__main__":
+    p = parser()
+    args = p.parse_args()
+    rv = main(args)
+    sys.exit(rv)
