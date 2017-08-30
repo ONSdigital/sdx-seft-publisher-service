f6daa6e24b7517ffdcb0005b0938e64bdfb2d041
Dave Haynes <david.haynes@ons.gov.uk>
2017-08-30 09:40:10 +0100

    Codacy fix.

--- app/main.py
+++ app/main.py
@@ -128,12 +128,13 @@ class Task:
         self.rabbit_check = None
         self.ftp_check = None
 
-    def check_services(self, ftp_params={}, rabbit_url=""):
+    def check_services(self, ftp_params=None, rabbit_url=""):
+        ftp_params = ftp_params or self.ftp_params(self.services)
         http_client = AsyncHTTPClient()
         params = self.amqp_params(self.services)
         self.rabbit_check = http_client.fetch(rabbit_url or params["check"])
 
-        ftp = FTPWorker(**(ftp_params or self.ftp_params(self.services)))
+        ftp = FTPWorker(**ftp_params)
         self.ftp_check = self.executor.submit(ftp.check)
 
     def transfer_files(self):
